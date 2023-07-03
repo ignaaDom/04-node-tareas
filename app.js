e2a1ee094b7a7e1c8bcf6676e0ab1cd6f71f3cfd
Ignacio <igna.dom1604@gmail.com>
2023-07-03 00:04:20 -0300

    Arreglado el tema del require con inquirer

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const colors = require('colors');
 const { inquirerMenu } = require('./helpers/inquirer');
-// const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 
 const main = async() =>{
@@ -9,10 +8,7 @@ const main = async() =>{
     let opcion = '';
 
     do{
-        // opcion = await mostrarMenu();
         opcion = await inquirerMenu();
-        console.log(opcion);
-        if(opcion !== '0') await pausa();
     }while(opcion !== '0');
 
 }
--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,4 +1,4 @@
-require('colors');
+const colors = require('colors');
 const inquirer = require('inquirer');
 
 const preguntas = [
@@ -6,7 +6,44 @@ const preguntas = [
         type: 'list',
         name: 'opcion',
         message: '¿Qué desea hacer?',
-        choices: ['opt1','opt2','opt3']
+        choices: [
+            {
+                value: '1',
+                name: '1. Crear tarea'
+            },
+            {
+                value: '2',
+                name: '2. Listar tareas'
+            },
+            {
+                value: '3',
+                name: '3. Listar tareas completadas'
+            },
+            {
+                value: '4',
+                name: '4. Listar taeas pendientes'
+            },
+            {
+                value: '5',
+                name: '5. Completar taeas'
+            },
+            {
+                value: '6',
+                name: '6. Borrar tarea'
+            },
+            {
+                value: '0',
+                name: '0. Salir'
+            }
+    ],
+    }
+];
+
+const preguntaPausa = [
+    {type: 'list',
+        name: 'opcion',
+        message: '¿Qué desea hacer?',
+        choices: ""
     }
 ];
 
@@ -14,12 +51,16 @@ const inquirerMenu = async()=>{
     console.clear();
 
     console.log('========================='.green);
-    console.log('  Seleccione una opcion  ');
+    console.log('  Seleccione una opcion  '.green);
     console.log('=========================\n'.green);
 
-    const opt  = await inquirer.createPromptModule([preguntas]);
+    const {opcion}  = await inquirer.prompt(preguntas);
 
-    return opt;
+    return opcion;
+}
+
+const pausa = ()=>{
+    
 }
 
 module.exports = {
